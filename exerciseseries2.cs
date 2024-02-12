d7ed4859930d0b76d20215f183e33a2b521c0409
Sujal Shrestha <sujalshrestha199@gmail.com>
2024-02-12 11:39:22 -0400

    first commit

---
+++ exerciseseries2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number == 0)
+        {
+            Console.WriteLine("The number is zero.");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else
+        {
+            Console.WriteLine("The number is negative.");
+        }
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is even.");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd.");
+        }
+    }
+}
